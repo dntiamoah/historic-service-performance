adf5660a1e39fb3ad4ab75ae442bfd45a292a387
dntiamoah <ntiamoahd@btinternet.com>
2025-12-22 21:37:48 +0000

    phase two loop on days, then schedule - loadAllAvailableHistoricServicePerformanceData

--- src/main/java/com/nafifsource/hsp/service_performance/batch/HistoricServicePerformanceLauncher.java
+++ src/main/java/com/nafifsource/hsp/service_performance/batch/HistoricServicePerformanceLauncher.java
@@ -34,8 +34,16 @@ public class HistoricServicePerformanceLauncher {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    @Scheduled(cron = "0 1 1 ? * *")
-    void launchMCALoader(LocalDate theDate) throws JobInstanceAlreadyCompleteException,
+//    * "0 0 * * * *" = the top of every hour of every day.
+//    * "*/10 * * * * *" = every ten seconds.
+//    * "0 0 8-10 * * *" = 8, 9 and 10 o'clock of every day.
+//    * "0 0 8,10 * * *" = 8 and 10 o'clock of every day.
+//    * "0 0/30 8-10 * * *" = 8:00, 8:30, 9:00, 9:30 and 10 o'clock every day.
+//    * "0 0 9-17 * * MON-FRI" = on the hour nine-to-five weekdays
+//    * "0 0 0 25 12 ?" = every Christmas Day at midnight
+
+    @Scheduled(cron = "0 0 22 * * *")
+    void loadAllAvailableHistoricServicePerformanceData(LocalDate theDate) throws JobInstanceAlreadyCompleteException,
             JobExecutionAlreadyRunningException,
             JobRestartException,
             InvalidJobParametersException {
